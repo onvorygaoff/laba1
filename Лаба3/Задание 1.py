8f9fe49a66b8adca5d9c04e6951af613116def31
onvorygaoff <vovarovsky@gmail.com>
2024-11-03 21:16:18 +0300

    Add files via upload

---
+++ Лаба3/Задание 1.py
@@ -0,0 +1,14 @@
+
+def find_item_index(items_list, item_to_find):
+    if item_to_find in items_list:
+        return items_list.index(item_to_find)
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_index(items_list, find_item)  # Вызов функции для поиска индекса
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
